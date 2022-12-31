921967c3a09eaa51a22824ce8e4749b0a98ca689
4idrossifenil-etanammide <saravalle.1948684@studenti.uniroma1.it>
2022-12-31 18:18:06 +0100

    Implementa login

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,13 +1,21 @@
+import axios from "axios";
 import {createRouter, createWebHashHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HomePageView from '../views/HomePageView.vue'
+
+axios.interceptors.request.use(function (config){
+		config.headers.Authorization = localStorage.getItem("token");
+		return config;
+	}, function (error) {
+		return Promise.reject(error);
+	}
+);
 
 const router = createRouter({
 	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
 		{path: '/', component: HomeView},
-		{path: '/link1', component: HomeView},
-		{path: '/link2', component: HomeView},
-		{path: '/some/:id/link', component: HomeView},
+		{path: '/homepage', component: HomePageView},
 	]
 })
 
--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -4,53 +4,103 @@ export default {
 		return {
 			errormsg: null,
 			loading: false,
-			some_data: null,
+			username: '',
+			response: {}
 		}
 	},
 	methods: {
-		async refresh() {
+		submit: async function () {
 			this.loading = true;
-			this.errormsg = null;
-			try {
-				let response = await this.$axios.get("/");
-				this.some_data = response.data;
-			} catch (e) {
-				this.errormsg = e.toString();
+			this.username = this.$refs.username.value;
+			try{
+				let identifier = await this.$axios.post("/session", {
+					name : this.username
+				})
+				this.response = identifier.data;
+				localStorage.setItem("token", this.response.id);
+			}catch (e) {
+
 			}
 			this.loading = false;
+			this.$router.push("/homepage");
 		},
 	},
-	mounted() {
-		this.refresh()
-	}
 }
 </script>
 
 <template>
-	<div>
-		<div
-			class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-			<h1 class="h2">Home page</h1>
-			<div class="btn-toolbar mb-2 mb-md-0">
-				<div class="btn-group me-2">
-					<button type="button" class="btn btn-sm btn-outline-secondary" @click="refresh">
-						Refresh
-					</button>
-					<button type="button" class="btn btn-sm btn-outline-secondary" @click="exportList">
-						Export
-					</button>
-				</div>
-				<div class="btn-group me-2">
-					<button type="button" class="btn btn-sm btn-outline-primary" @click="newItem">
-						New
-					</button>
-				</div>
-			</div>
+	<h1 class="page-title">WASAPhoto</h1>
+	<div class = "centering-container">
+		<div class="container">
+			<h1 class="login-title">Login</h1>
+			<form class="login-form" @submit.prevent="submit">
+				<label for="username">Username:</label>
+				<input v-model="username" type="text" ref="username" placeholder="Insert username..." required>
+				<button>Login</button>
+			</form>
+			<h4 v-if="!loading">{{ this.response.id }}</h4>
 		</div>
-
-		<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
 	</div>
 </template>
 
 <style>
+
+.centering-container {
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+}
+
+.page-title{
+	position: absolute;
+	top: 10%;
+	left: 50%;
+	transform: translateX(-50%);
+}
+
+.container {
+	width: 400px;
+	height: 400px;
+	background-color: #666262;
+	display: flex; /* Serve per inserire gli elementi nel container aggiustando la loro dimensione per entrare dentro i bound prima definiti */
+	flex-direction: column; /* Direzione in cui vengono piazzati i componenti */
+	align-items: center; /* Come vengono allineati i componenti, cioè nel centro rispetto al main axis (column prima definito) */
+	justify-content: center; /* Come i componenti vengono piazzati uno rispetto all'altro, in questo caso sono distruibuti centralmente rispetto al main axis */
+	border-radius: 5px;
+}
+ 
+.login-title{
+	text-align: center;
+}
+
+.login-form{
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.login-form label{
+	margin-bottom: 10px;
+}
+
+.login-form input{
+	width: 100%; /* Occupa tutto lo spazio del componente genitore */
+	padding: 12px 20px; /* Specifica lo spazio lasciato a partire dai bordi del componente. In questo caso, top e bottom di 12 e right left di 20 */
+	margin: 8px 0; /* Crea spazio ATTORNO ai componenti, quindi top e bottom di 8px e left e right di 0px */
+	box-sizing: border-box; /* Include il padding e il maring nel calcolo del width e height */
+	border: 1px solid #ccc;
+	border-radius: 4px; /* Aggiunge spigoli arrotondati */
+}
+
+.login-form button{
+	width: 100%;
+	background-color: #4caf50;
+	color: white;
+	padding: 14px 20px;
+	margin: 8px 0;
+	border: none;
+	border-radius: 4px;
+	cursor: pointer;
+}
 </style>
